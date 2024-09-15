d357c678e10938db644514c46852d87ac9145ad4
Rayirth Soni <rayirth.soni@finbox.in>
2024-09-15 19:32:27 +0530

    update: function for pokemon stats

--- scraping/helpers.py
+++ scraping/helpers.py
@@ -16,9 +16,9 @@ def extract_table_data(**kwargs) -> Tag:
     """
     soup = kwargs.get("soup")
     tag = kwargs.get("tag")
-    id = kwargs.get("  id")
+    data_id = kwargs.get("data_id")
 
-    return soup.find(tag, id=id)
+    return soup.find(tag, id=data_id)
 
 
 def clean_name(text: str) -> str:
@@ -39,10 +39,10 @@ def clean_name(text: str) -> str:
 
 
 def extract_pokemon_stats(**kwargs) -> list:
-    """Function is used to extract pokemon stats from table
+    """Function is used to extract pokémon stats from table
 
     Returns:
-        list: List of dictionaries containing pokemon stats
+        list: List of dictionaries containing pokémon stats
     """
     soup = kwargs.get("soup")
     tag = kwargs.get("tag")
@@ -53,23 +53,23 @@ def extract_pokemon_stats(**kwargs) -> list:
     for num, cell in enumerate(soup.find_all(tag)):
         if cell.get("class") in target_cell_classes:
             if num % 10 == 1:
-                pokemon_data['name'] = clean_name(cell.text.strip())
+                pokemon_data["name"] = clean_name(cell.text.strip())
             elif num % 10 == 2:
-                pokemon_data['type'] = cell.text.strip()
+                pokemon_data["type"] = cell.text.strip()
             elif num % 10 == 3:
-                pokemon_data['total'] = int(cell.text.strip())
+                pokemon_data["total"] = int(cell.text.strip())
             elif num % 10 == 4:
-                pokemon_data['hp'] = int(cell.text.strip())
+                pokemon_data["hp"] = int(cell.text.strip())
             elif num % 10 == 5:
-                pokemon_data['attack'] = int(cell.text.strip())
+                pokemon_data["attack"] = int(cell.text.strip())
             elif num % 10 == 6:
-                pokemon_data['defense'] = int(cell.text.strip())
+                pokemon_data["defense"] = int(cell.text.strip())
             elif num % 10 == 7:
-                pokemon_data['sp_attack'] = int(cell.text.strip())
+                pokemon_data["sp_attack"] = int(cell.text.strip())
             elif num % 10 == 8:
-                pokemon_data['sp_defense'] = int(cell.text.strip())
+                pokemon_data["sp_defense"] = int(cell.text.strip())
             elif num % 10 == 9:
-                pokemon_data['speed'] = int(cell.text.strip())
+                pokemon_data["speed"] = int(cell.text.strip())
                 response.append(pokemon_data)
                 pokemon_data = {}
     return response
--- scraping/scrape.py
+++ scraping/scrape.py
@@ -19,10 +19,26 @@ POKEMON_DATA = Constants.POKEMON_DATA
 POKEMON_STATS_URL = Constants.POKEMON_STATS_URL
 
 
-def scrape_pokemon_stats():
+def scrape_pokemon_stats() -> list:
+    """Function to scrape pokemon stats from pokemondb.net
+
+    Args:
+        None
+
+    Returns:
+        list: List of dictionaries containing pokemon stats
+    """
     pokemon_stats_response = requests.get(POKEMON_STATS_URL)
     pokemon_site_data = pokemon_stats_response.text
     soup = BeautifulSoup(pokemon_site_data, "lxml")
-    pokemon_stats_table = scraping_helpers.extract_table_data(
-        soup=soup, tag="table", id="pokedex"
+    response = scraping_helpers.extract_pokemon_stats(
+        soup=soup,
+        tag="td",
+        target_cell_classes=[
+            ["cell-name"],
+            ["cell-icon"],
+            ["cell-num", "cell-total"],
+            ["cell-num"],
+        ],
     )
+    return response
