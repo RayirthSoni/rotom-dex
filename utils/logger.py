e59d6a0fbf88efaecadb898008c158dc9981049b
Rayirth Soni <rayirthsoni@Rayirths-MacBook-Pro.local>
2024-10-12 21:37:28 +0530

    udpate: added logger

--- utils/logger.py
+++ utils/logger.py
@@ -11,3 +11,11 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(name="pokelogger")
+
+log_handler = logging.StreamHandler()
+log_handler.setLevel(logging.INFO)  # You can set the desired log level for console output
+
+log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+log_handler.setFormatter(log_formatter)
+
+logger.addHandler(log_handler)
