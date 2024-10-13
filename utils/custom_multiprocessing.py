1a5b3470ff8ec64c0389860b92a10c906c81c1a6
Rayirth Soni <rayirthsoni@Rayirths-MacBook-Pro.local>
2024-10-13 12:42:31 +0530

    udpate: added process and thread executor

--- utils/custom_multiprocessing.py
+++ utils/custom_multiprocessing.py
@@ -6,13 +6,18 @@ from concurrent import futures
 
 
 class ProcessExecutor:
+    """Class to handle multiprocessing
+    """
     def __init__(self):
         self.executor = futures.ProcessPoolExecutor()
 
     def __del__(self):
         self.executor.shutdown()
 
-    pass
+    def wait_on_futures(self, futures_iter):
+        futures.wait(futures_iter)
 
+    def submit(self, fn, **kwargs):
+        return self.executor.submit(fn, **kwargs)
 
 executor = ProcessExecutor()
--- utils/custom_threading.py
+++ utils/custom_threading.py
@@ -3,20 +3,26 @@ Script contains functions for threading
 """
 
 from concurrent import futures
+from utils.logger import logger
 
 
 class ThreadExecutor:
+    """
+    Class to handle threading
+    """
     def __init__(self):
         self.executor = futures.ThreadPoolExecutor()
 
     def __del__(self):
         self.executor.shutdown()
 
-    def wait_on_futures(self):
-        pass
+    def wait_on_futures(self, futures_iter):
+        logger.info("ThreadExecutor: Waiting on futures")
+        futures.wait(futures_iter)
+        logger.info("ThreadExecutor: done waiting")
 
     def submit(self, fn, **kwargs):
-        self.executor.submit(fn, **kwargs)
+        return self.executor.submit(fn, **kwargs)
 
 
 executor = ThreadExecutor()
